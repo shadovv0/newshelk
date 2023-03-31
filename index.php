70db53e2ed092f2972b0fcbc0d709491702f118c
shadow <shadovv39@gmail.com>
2023-03-31 11:01:32 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Щелковская/Главная</title>
+    <meta name="description" content="Описание страницы"/>
+
+    <link rel="stylesheet" type="text/css" href="/css/style.css"/>
+    <!--    <link rel="icon" href="/img/favicon.png">-->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+
+    <!--<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>-->
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
+        crossorigin="anonymous"></script>
+<header>
+    <div class="header-main">
+        <div class="logo">
+            ЛОГО/Название сайта
+        </div>
+        <div class="phone">
+            (499) 123-34-56
+        </div>
+        <div class="top-menu">
+            <div class="wrap clear">
+                <div class="top-menu__wrap">
+                <div class="top-menu-btn">
+                    <ul class="usluga1">
+                        <a href="" class="root-item1">Клиники</a>
+                    </ul>
+                    <ul class="usluga2">
+                        <a href="" class="root-item2">Врачи</a>
+                    </ul>
+                    <ul class="usluga3">
+                        <a href="" class="root-item3">Услуги</a>
+                    </ul>
+                    <ul class="usluga4">
+                        <a href="" class="root-item4">Диагностика</a>
+                    </ul>
+                    <ul class="usluga5">
+                        <a href="" class="root-item5">Заболевания</a>
+                    </ul>
+                    <ul class="usluga6">
+                        <a href="" class="root-item6">Симптомы</a>
+                    </ul>
+                </div>
+                <div class="search-form" style="width: 324px">
+                    <form name="search-form" action="/search/" method="GET" style="width: 324px">
+                        <input class="js-search-input" type="text" placeholder="Поиск" name="q" value=""/>
+                        <button class="search-btn" href="#" ></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="content">
+    <div class="items">
+        <div class="sort">
+            <a href="" class="sorting">Популярность ↑ ↓</a>
+            <a href="" class="sorting">Рейтинг ↑ ↓</a>
+            <a href="" class="sorting">Количество отзывов ↑ ↓</a>
+        </div>
+        <!--<div class="clinics-items">-->
+        <div class="clinics-container">
+            <div class="photo">
+                <img src="img/clinic.png" style="max-width: 50%">
+                <div class="percent-human">
+                    <img src="img/chel.png" style="max-width: 15px; margin-right: 5px">
+                    <a>56 врачей в штате</a>
+                </div>
+                <div class="stars">
+                    <img src="img/star.png" style="max-width: 25px">
+                    <a class="percent-review">4,7</a>
+                </div>
+                <div class="reviews">
+                    14 отзывов
+                </div>
+            </div>
+            <div class="clinic-info">
+                <div class="clinic-info-left">
+                    <div class="clinic-name">
+                        <p>
+                            Семейная клиника
+                        </p>
+                    </div>
+                    <div class="clinic-type">
+                        <p>Многопрофильная медицинская клиника, г .Москва</p>
+                    </div>
+                    <div class="clinic-adress">
+                        <p>Москва, м. Щелковская, ул. Флотская, д.42</p>
+                    </div>
+                    <div class="clinic-text-block">
+                        <p>Многопрофильная медицинская клиника в Москве.<br>
+                            Наш центр предоставляет полный перечень медицинских услуг<br>
+                            и консультации лучших специалистов.</p>
+                    </div>
+                </div>
+
+                <div class="clinic-info right">
+                    <div class="clinic-schedule">
+                        <p>Часы приёма:<br>
+                            Пн-Пт 10:00-20:00 Сб-Вс 10:00-18:00
+                        </p>
+                    </div>
+                    <div class="clinic-metro">
+                        Текстильщики (338м)
+                        <div class="clinic-metro-further">
+                            Печатники (2,1км) Стахановская (2,2км)
+                        </div>
+                    </div>
+                    <div class="clinic-phone">
+                        <img src="img/phone.png" style="max-width: 100px">
+                        (499)123-45-67
+                    </div>
+                    <div class="btn-zapis-napriem">
+                        <span class="button-record">Запись на приём</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="clinics-container">
+            <div class="photo">
+                <img src="img/clinic.png" style="max-width: 50%">
+                <div class="percent-human">
+                    <img src="img/chel.png" style="max-width: 15px; margin-right: 5px">
+                    <a>56 врачей в штате</a>
+                </div>
+                <div class="stars">
+                    <img src="img/star.png" style="max-width: 25px">
+                    <a class="percent-review">4,7</a>
+                </div>
+                <div class="reviews">
+                    14 отзывов
+                </div>
+            </div>
+            <div class="clinic-info">
+                <div class="clinic-info-left">
+                    <div class="clinic-name">
+                        <p>
+                            Семейная клиника
+                        </p>
+                    </div>
+                    <div class="clinic-type">
+                        <p>Многопрофильная медицинская клиника, г .Москва</p>
+                    </div>
+                    <div class="clinic-adress">
+                        <p>Москва, м. Щелковская, ул. Флотская, д.42</p>
+                    </div>
+                    <div class="clinic-text-block">
+                        <p>Многопрофильная медицинская клиника в Москве.<br>
+                            Наш центр предоставляет полный перечень медицинских услуг<br>
+                            и консультации лучших специалистов.</p>
+                    </div>
+                </div>
+
+                <div class="clinic-info right">
+                    <div class="clinic-schedule">
+                        <p>Часы приёма:<br>
+                            Пн-Пт 10:00-20:00 Сб-Вс 10:00-18:00
+                        </p>
+                    </div>
+                    <div class="clinic-metro">
+                        Текстильщики (338м)
+                        <div class="clinic-metro-further">
+                            Печатники (2,1км) Стахановская (2,2км)
+                        </div>
+                    </div>
+                    <div class="clinic-phone">
+                        <img src="img/phone.png" style="max-width: 100px">
+                        (499)123-45-67
+                    </div>
+                    <div class="btn-zapis-napriem">
+                        <span class="button-record">Запись на приём</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="clinics-container">
+            <div class="photo">
+                <img src="img/clinic.png" style="max-width: 50%">
+                <div class="percent-human">
+                    <img src="img/chel.png" style="max-width: 15px; margin-right: 5px">
+                    <a>56 врачей в штате</a>
+                </div>
+                <div class="stars">
+                    <img src="img/star.png" style="max-width: 25px">
+                    <a class="percent-review">4,7</a>
+                </div>
+                <div class="reviews">
+                    14 отзывов
+                </div>
+            </div>
+            <div class="clinic-info">
+                <div class="clinic-info-left">
+                    <div class="clinic-name">
+                        <p>
+                            Семейная клиника
+                        </p>
+                    </div>
+                    <div class="clinic-type">
+                        <p>Многопрофильная медицинская клиника, г .Москва</p>
+                    </div>
+                    <div class="clinic-adress">
+                        <p>Москва, м. Щелковская, ул. Флотская, д.42</p>
+                    </div>
+                    <div class="clinic-text-block">
+                        <p>Многопрофильная медицинская клиника в Москве.<br>
+                            Наш центр предоставляет полный перечень медицинских услуг<br>
+                            и консультации лучших специалистов.</p>
+                    </div>
+                </div>
+
+                <div class="clinic-info right">
+                    <div class="clinic-schedule">
+                        <p>Часы приёма:<br>
+                            Пн-Пт 10:00-20:00 Сб-Вс 10:00-18:00
+                        </p>
+                    </div>
+                    <div class="clinic-metro">
+                        Текстильщики (338м)
+                        <div class="clinic-metro-further">
+                            Печатники (2,1км) Стахановская (2,2км)
+                        </div>
+                    </div>
+                    <div class="clinic-phone">
+                        <img src="img/phone.png" style="max-width: 100px">
+                        (499)123-45-67
+                    </div>
+                    <div class="btn-zapis-napriem">
+                        <span class="button-record">Запись на приём</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--</div>-->
+<footer class="footer">
+</footer>
+</body>
+</html>
