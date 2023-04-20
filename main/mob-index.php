89bbcebe58fd91ad73fa29c4c8e78e28e811ee5e
shadow <shadovv39@gmail.com>
2023-04-20 17:50:41 +0200

    rename

--- main/mob-index.php
+++ main/mob-index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Щелковская/Главная</title>
     <meta name="description" content="Описание страницы"/>
-    <link rel="stylesheet" type="text/css" href="/css/main/mob-style.css"/>
+    <link rel="stylesheet" type="text/css" href="/css/main/styles_mobile.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
