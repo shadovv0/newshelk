dbe8b25ebefccc8cc6a9064cbfd7c8a425d9c06d
nikcrysis <nikcrysis@mail.ru>
2023-04-21 13:25:23 +0200

    index file

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Щелковская/Главная</title>
+    <meta name="description" content="Описание страницы"/>
+
+    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+<body>
+    <div>
+        <a href="/main/index.php">Главная Десктоп</a>
+        <br>
+        <a href="/main/mob-index.php">Главная Мобилка</a>
+
+
+        <hr>
+
+        <a href="/clinic/index.php">Клиника Десктоп</a>
+        <br>
+        <a href="/clinic/mob-index.php">Клиника Мобилка</a>
+
+        <hr>
+
+        <a href="/doctor/index.php">Врач Десктоп</a>
+        <br>
+        <a href="/doctor/mob-index.php">Врач Мобилка</a>
+    </div>
+</body>
+</html>
